94f0a393954dfb36538ace3de6a1a5e6b30c64ff
LeToleau <oliveriobaroni@gmail.com>
2024-05-29 21:35:26 -0300

    42 form fields validations corrected and prize assignment on server side

--- lb-contest-front/src/components/FormQ.jsx
+++ lb-contest-front/src/components/FormQ.jsx
@@ -1,8 +1,8 @@
 import Bgd from '../assets/img/main-bgd.png';
 import PlayBtn from './buttons/Button';
-import { /*useEffect,*/ useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import axios from 'axios';
 import '../assets/scss/components/Form.scss';
 
 function Form() {
@@ -10,7 +10,7 @@ function Form() {
   const [formData, setFormData] = useState({
     name: '',
     lastname: '',
-    email: '',
+    mail: '',
     phone: '',
     taxCode: '',
     city: '',
@@ -22,14 +22,14 @@ function Form() {
 
   const navigate = useNavigate()
   
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
    e.preventDefault();
 
    const isValidated = Object.values(formData).some(value => value === '');
    let validations = !isValidated;
 
    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-   const validMail = emailRegex.test(formData.email);
+   const validMail = emailRegex.test(formData.mail);
 
    const postalCodeRegex = /^\d{5}$/;
    const validPostalCode = postalCodeRegex.test(formData.postCode);
@@ -60,23 +60,39 @@ function Form() {
    const conditionKey = Object.keys(conditions).find(key => conditions[key]);
     
     if (conditionKey === 'valid') {
-      // Reiniciar el estado del formulario después de enviar los datos
       setValidationMsg(validationMessages.valid);
-      setFormData({
-        name: '',
-        lastname: '',
-        email: '',
-        phone: '',
-        taxCode: '',
-        city: '',
-        postCode: '',
-        province: '',
-        address: '',
-        termsConditions: false,
-      });
+      // document.querySelector('.form').style.opacity = 0;
+
+      try {
+        const response = await axios.post('http://localhost:3000/api/participants', formData);
+        if (response.status === 201) {
+          console.log('Participante agregado con éxito:', response.data);
+          // Reiniciar el estado del formulario después de enviar los datos
+          setFormData({
+            name: '',
+            lastname: '',
+            mail: '',
+            phone: '',
+            taxCode: '',
+            city: '',
+            postCode: '',
+            province: '',
+            address: '',
+            termsConditions: false,
+          });
+        } else {
+          console.error('Error al agregar participante', response.data);
+        }
+      } catch (error) {
+        console.error('Error al enviar la solicitud', error);
+      }
+
+
+      /*
       setTimeout(() => {
-        navigate('/quasi')
+        navigate('/win-page')
       }, 200);
+      */
     }  else {
       setValidationMsg(validationMessages[conditionKey]);
     }
@@ -106,29 +122,11 @@ function Form() {
       backgroundImage: `url(${Bgd})`,
   };
 
-  /*
   useEffect(() => {
     setTimeout(()=>{
-      document.querySelector('.main-background__logo').style.transform = `translate(0px, 0px)`;
-      document.querySelector('.button').style.transform = `translate(0px, 0px)`;
-      document.querySelector('.main-background').style.opacity = 1;
-      document.querySelector('.main-background__logo').style.opacity = 1;
-      document.querySelector('.button').style.opacity = 1;
+      document.querySelector('.form__form').style.opacity = 1;
     }, 100)
   }, [])
-  */
-
-  /*
-  const handlePlayClick = () => {
-    document.querySelector('.main-background__logo').style.opacity = 0;
-    document.querySelector('.main-background').style.opacity = 0;
-    document.querySelector('.button').style.opacity = 0;
-    
-    setTimeout(() => {
-      navigate('/game')
-    }, 200);
-  };
-  */
 
   return (
     <div className="form" style={estiloDelDiv}>
@@ -163,11 +161,11 @@ function Form() {
               onChange={handleChange}
             />
             <input
-              id="email"
-              name="email"
-              type="email"
+              id="mail"
+              name="mail"
+              type="mail"
               placeholder="Email"
-              value={formData.email}
+              value={formData.mail}
               onChange={handleChange}
             />
           </div>
